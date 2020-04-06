59acea59461a5ef16c54723fd87da34176bdba5a
judahjohnson <62618989+judahjohnson@users.noreply.github.com>
2020-04-05 21:21:51 -0500

    Delete vector_similarity.py

--- modules/vector_similarity.py
+++
@@ -1,8 +0,0 @@
-def vector_similarity(vector1,vector2):
-	import numpy as np
-	if (np.linalg.norm(vector1)*np.linalg.norm(vector2)) !=0:
-		cosine_angle=np.inner(vector1,vector2)/(np.linalg.norm(vector1)*np.linalg.norm(vector2))
-	else:
-		cosine_angle=0
-		
-	return cosine_angle
